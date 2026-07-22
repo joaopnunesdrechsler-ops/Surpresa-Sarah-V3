442ce49b445220ece924eff5a0631207ba7f838a
Joao Paulo Nunes <joaopnunesdrechsler@email.com>
2026-07-21 23:11:50 -0300

    adicionar pagina e historia na surpresa

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function abrirSurpresa(){
-
-    document.getElementById("entrada").style.display="none";
+let paginaAtual = 0;
 
 
-    document
-    .getElementById("surpresa")
-    .classList
-    .remove("escondido");
+const paginas = document.querySelectorAll(".pagina");
+
+
+function proximaPagina(){
+
+
+paginas[paginaAtual].classList.remove("ativa");
+
+
+paginaAtual++;
+
+
+if(paginaAtual < paginas.length){
+
+paginas[paginaAtual].classList.add("ativa");
+
+}
+
 
 }
