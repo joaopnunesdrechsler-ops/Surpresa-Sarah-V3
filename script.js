ea70a560e2417270bdb71063322727569406dc63
Joao Paulo Nunes <joaopnunesdrechsler@email.com>
2026-07-21 22:50:34 -0300

    nova versao viasual da surpresa

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function abrirSurpresa(){
 
-    document.getElementById("entrada").style.display = "none";
+    document.getElementById("entrada").style.display="none";
 
-    document.getElementById("surpresa")
-    .classList.remove("escondido");
+
+    document
+    .getElementById("surpresa")
+    .classList
+    .remove("escondido");
+
 
 }
